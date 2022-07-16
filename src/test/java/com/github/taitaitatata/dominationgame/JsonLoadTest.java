31d7dda35f3a8e535bbccf634d376120a5b28caf
Taitaitatata <taitaitatatasub@outlook.jp>
2022-07-16 12:35:57 +0900

    [Add] Jsonロードできるかテストするための何か

---
+++ src/test/java/com/github/taitaitatata/dominationgame/JsonLoadTest.java
@@ -0,0 +1,36 @@
+package com.github.taitaitatata.dominationgame;
+
+import com.google.gson.Gson;
+import com.github.taitaitatata.dominationgame.file.Arena;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonLoadTest {
+
+    @Test
+    public void loadTest() {
+        ClassLoader cl = getClass().getClassLoader();
+        Gson gson = new Gson();
+
+        try {
+            File file = new File(cl.getResource("arena1.json").getFile());
+
+            Arena a = gson.fromJson(
+                    Files.newBufferedReader(file.toPath()),
+                    Arena.class
+            );
+
+            System.out.println(a.getDisplayName());
+        } catch (IOException | JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
